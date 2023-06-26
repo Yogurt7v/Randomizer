2c6841f1f3dacb433c609db483d01ed5517257d6
Yogurt7vgit config --global user.name Yogurt7v <egorov-2k@yandex.rugit config --global user.name Yogurt7v>
2023-06-26 20:26:23 +0300

    осталось проверку на сумму и проверку на выходные.

--- index.js
+++ index.js
@@ -3,16 +3,17 @@
 // let random = document.querySelector("random");
 
 // let startBtn = document.querySelector("start-btn");
+// let result = document.querySelector("#result");
 
 // let sumValue = sum.value;
 
 // пока очень тупо, но поробуем сделать для начала функции//
-// let sum = prompt("введите сумму документа");
-// console.log(sum);
-// let size = prompt("введите количество документов");
-// console.log(size);
-// let random = prompt("введите разлёт в процентах");
-// console.log(random);
+let sum = prompt("введите сумму документа");
+console.log(sum);
+let size = prompt("введите количество документов");
+console.log(size);
+let random = prompt("введите разлёт в процентах");
+console.log(random);
 
 // переменные для случайной даты
 let startDate = prompt("введите стартовую дату (Год, Месяц, День.)");
@@ -27,15 +28,15 @@ let lastDateUniCode = lastDateUni.getTime();
 
 // функция случайной даты//
 
-let randomDateUnicode = Math.floor(
-  Math.random() * (lastDateUniCode - startDateUniCode + 1) + startDateUniCode
-);
-let randomDate = new Date(randomDateUnicode);
-console.log(randomDate);
+// console.log(randomDate);
 
 // Генератор даты - Работает//
 
 function generateDate() {
+  let randomDateUnicode = Math.floor(
+    Math.random() * (lastDateUniCode - startDateUniCode + 1) + startDateUniCode
+  );
+  let randomDate = new Date(randomDateUnicode);
   date = new Date(randomDate);
   let day = date.getDate();
   if (date.toString().startsWith("Sat")) {
@@ -43,17 +44,14 @@ function generateDate() {
   } else if (date.toString().startsWith("Sun")) {
     day = day + 1;
   }
-  console.log(day);
   if (day < 10) {
     day = "0" + day;
   }
-  if (date.getMonth() < 10) {
+  if (date.getMonth() < 9) {
     month = "0" + (date.getMonth() + 1);
   } else month = date.getMonth() + 1;
   return (date = `${day}:${month}:${date.getFullYear()}`);
 }
-generateDate(); // проверка работы функции
-console.log(date); // проверка работы функции
 
 // функция генерирования суммы без копеек = работает//
 
@@ -65,5 +63,28 @@ let firstDoc = Math.floor(Math.random() * (maxDoc - minDoc + 1) + minDoc);
 
 // функция генерирования суммы с копеками = работает//
 
-let secondDoc =
-  Math.floor((Math.random() * (maxDoc - minDoc) + minDoc) * 100) / 100;
+function secondDoc() {
+  return Math.floor((Math.random() * (maxDoc - minDoc) + minDoc) * 100) / 100;
+}
+
+// заверашающая функция пока без копеек//
+function randomize() {
+  let finalSum = 0;
+  for (let i = 0; i < size; i++) {
+    let finalValue = secondDoc();
+    let finalDate = generateDate();
+    let invalidDate = new Date(finalDate);
+    if (invalidDate == "Invalid Date") {
+      finalDate = generateDate();
+    }
+    // finalSum = finalSum + finalValue; // dont work
+    // if (finalSum > sum) {
+    //   finalValue = finalSum - (finalSum - sum);
+    // } else if (finalSum < sum) {
+    //   finalValue = finalValue + (sum - finalSum);
+    // }
+    console.log("Дата " + finalDate + "\n" + "Сумма документа", finalValue);
+    console.log(finalSum);
+  }
+}
+randomize();
