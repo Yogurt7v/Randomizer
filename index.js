47ca815cdc008a5c6f3f982ab8175274599e58ae
Yogurt7vgit config --global user.name Yogurt7v <egorov-2k@yandex.rugit config --global user.name Yogurt7v>
2023-06-28 22:14:53 +0300

    работает с кнопки

--- index.js
+++ index.js
@@ -1,91 +1,98 @@
-// let sum = document.querySelector("sum");
-// let size = document.querySelector("size");
-// let random = document.querySelector("random");
+let sumValue = document.querySelector(`.sum`);
+let sizeValue = document.querySelector(`.size`);
+let randomValue = document.querySelector(`.random`);
+let startDateValue = document.querySelector(`.startDate`);
+let lastDateValue = document.querySelector(`.lastDate`);
 
-// let startBtn = document.querySelector("start-btn");
-// let result = document.querySelector("#result");
+let startBtn = document.querySelector(`button`);
+let result = document.querySelector(`.result`);
 
-// let sumValue = sum.value;
+startBtn.onclick = function () {
+  let sum = sumValue.value;
+  let size = sizeValue.value;
+  let random = randomValue.value;
+  let startDate = startDateValue.value;
+  let lastDate = lastDateValue.value;
 
-// пока очень тупо, но поробуем сделать для начала функции//
-let sum = prompt("введите сумму документа");
-console.log(sum);
-let size = prompt("введите количество документов");
-console.log(size);
-let random = prompt("введите разлёт в процентах");
-console.log(random);
+  // // переменные для случайной даты
+  // let startDate = prompt("введите стартовую дату (Год, Месяц, День.)");
+  // console.log(startDate);
+  // let lastDate = prompt("введите конечную дату(Год, Месяц, День,)");
+  // console.log(lastDate);
 
-// переменные для случайной даты
-let startDate = prompt("введите стартовую дату (Год, Месяц, День.)");
-console.log(startDate);
-let lastDate = prompt("введите конечную дату(Год, Месяц, День,)");
-console.log(lastDate);
+  let startDateUni = new Date(startDate);
+  let startDateUniCode = startDateUni.getTime();
+  let lastDateUni = new Date(lastDate);
+  let lastDateUniCode = lastDateUni.getTime();
 
-let startDateUni = new Date(startDate);
-let startDateUniCode = startDateUni.getTime();
-let lastDateUni = new Date(lastDate);
-let lastDateUniCode = lastDateUni.getTime();
+  // функция случайной даты//
 
-// функция случайной даты//
+  // console.log(randomDate);
 
-// console.log(randomDate);
+  // Генератор даты - Работает//
 
-// Генератор даты - Работает//
-
-function generateDate() {
-  let randomDateUnicode = Math.floor(
-    Math.random() * (lastDateUniCode - startDateUniCode + 1) + startDateUniCode
-  );
-  let randomDate = new Date(randomDateUnicode);
-  date = new Date(randomDate);
-  let day = date.getDate();
-  if (date.toString().startsWith("Sat")) {
-    day = day + 2;
-  } else if (date.toString().startsWith("Sun")) {
-    day = day + 1;
-  }
-  if (day < 10) {
-    day = "0" + day;
-  }
-  if (day > 31) {
-    day = 31;
-  }
-  if (date.getMonth() <= 9) {
-    month = "0" + (date.getMonth() + 1);
-  } else month = date.getMonth() + 1;
-  return (date = `${day}:${month}:${date.getFullYear()}`);
-}
-
-// функция генерирования суммы без копеек = работает//
-
-let defaultDoc = sum / size;
-let minDoc = defaultDoc - defaultDoc * (random / 100);
-let maxDoc = defaultDoc + defaultDoc * (random / 100);
-
-let firstDoc = Math.floor(Math.random() * (maxDoc - minDoc + 1) + minDoc);
-
-// функция генерирования суммы с копеками = работает//
-
-function secondDoc() {
-  return Math.floor((Math.random() * (maxDoc - minDoc) + minDoc) * 100) / 100;
-}
-
-// заверашающая функция с копейками и посчётом остатков - работает//
-function randomize() {
-  let finalSum = 0;
-
-  for (let i = 0; i < size - 1; i++) {
-    let finalValue = secondDoc();
-    let finalDate = generateDate();
-    let invalidDate = new Date(finalDate);
-    if (invalidDate == "Invalid Date") {
-      finalDate = generateDate();
+  function generateDate() {
+    let randomDateUnicode = Math.floor(
+      Math.random() * (lastDateUniCode - startDateUniCode + 1) +
+        startDateUniCode
+    );
+    let randomDate = new Date(randomDateUnicode);
+    date = new Date(randomDate);
+    let day = date.getDate();
+    if (date.toString().startsWith("Sat")) {
+      day = day + 2;
+    } else if (date.toString().startsWith("Sun")) {
+      day = day + 1;
     }
-    finalSum = finalSum + finalValue;
-    console.log("Дата " + finalDate + "\n" + "Сумма документа", finalValue);
+    if (day < 10) {
+      day = "0" + day;
+    }
+    if (day > 31) {
+      day = 31;
+    }
+    if (date.getMonth() < 9) {
+      month = "0" + (date.getMonth() + 1);
+    } else month = date.getMonth() + 1;
+    return (date = `${day}:${month}:${date.getFullYear()}`);
   }
-  let endDate = generateDate();
-  let endValue = Math.floor((sum - finalSum) * 100) / 100;
-  console.log("Дата " + endDate + "\n" + "Сумма документа", endValue);
-}
-randomize();
+
+  // функция генерирования суммы без копеек = работает//
+
+  let defaultDoc = sum / size;
+  let minDoc = defaultDoc - defaultDoc * (random / 100);
+  let maxDoc = defaultDoc + defaultDoc * (random / 100);
+
+  let firstDoc = Math.floor(Math.random() * (maxDoc - minDoc + 1) + minDoc);
+
+  // функция генерирования суммы с копеками = работает//
+
+  function secondDoc() {
+    return Math.floor((Math.random() * (maxDoc - minDoc) + minDoc) * 100) / 100;
+  }
+
+  // заверашающая функция с копейками и посчётом остатков - работает//
+  function randomize() {
+    let finalSum = 0;
+
+    for (let i = 0; i < size - 1; i++) {
+      let finalValue = secondDoc();
+      let finalDate = generateDate();
+      let invalidDate = new Date(finalDate);
+      if (invalidDate == "Invalid Date") {
+        finalDate = generateDate();
+      }
+      finalSum = finalSum + finalValue;
+      console.log("Дата " + finalDate + "\n" + "Сумма документа", finalValue);
+    }
+    let endDate = generateDate();
+    let endValue = Math.floor((sum - finalSum) * 100) / 100;
+    console.log("Дата " + endDate + "\n" + "Сумма документа", endValue);
+  }
+
+  randomize();
+};
+
+/* добавить кнопку очистки
+  добавить проверку на выходные
+  добавить вывод результа 
+*/
